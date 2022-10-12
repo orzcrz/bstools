9492bcd1d2d04609937f932b4c326151f92928fc
crzorz <crzorz@outlook.com>
2022-10-12 17:01:11 +0800

    🔨 logger输出调整

--- shells/logger.sh
+++ shells/logger.sh
@@ -8,17 +8,19 @@ function log() {
   local logtype=$1
   local msg=$2
   local datetime=$(date +'%F %H:%M:%S')
-  local logformat="[${logtype}] ${datetime} [${FUNCNAME[2]} - $(caller 0 | awk '{print$1}')] ${msg}"
+  logformat_base="[${logtype}] ${datetime} ${msg}"
+  logformat_detail="[${logtype}] ${datetime} [${FUNCNAME[2]} - $(caller 0 | awk '{print$1}')] ${msg}"
+
   {
   case $logtype in
   DEBUG)
-    [[ $loglevel -le 0 ]] && echo -e "\033[34m${logformat}\033[0m" ;;
+    [[ $loglevel -le 0 ]] && echo -e "\033[37m${logformat_detail}\033[0m" ;;
   INFO)
-    [[ $loglevel -le 1 ]] && echo -e "\033[32m${logformat}\033[0m" ;;
+    [[ $loglevel -le 1 ]] && echo -e "\033[32m${logformat_base}\033[0m" ;;
   WARNING)
-    [[ $loglevel -le 2 ]] && echo -e "\033[33m${logformat}\033[0m" ;;
+    [[ $loglevel -le 2 ]] && echo -e "\033[33m${logformat_detail}\033[0m" ;;
   ERROR)
-    [[ $loglevel -le 3 ]] && echo -e "\033[31m${logformat}\033[0m" ;;
+    [[ $loglevel -le 3 ]] && echo -e "\033[31m${logformat_detail}\033[0m" ;;
   esac
   } #| tee -a $logfile
 }
