926f5f52a4100ebf82c4d9f7a42ec62e6acd9426
crzorz <crzorz@outlook.com>
2022-10-26 14:44:41 +0800

    update plugin folder generator scripts

--- misc/plugin_gen.sh
+++ misc/plugin_gen.sh
@@ -3,14 +3,16 @@
 # Created by crzorz on 2022/09/16
 # Copyright © 2022 BaldStudio. All rights reserved.
 
+## ./plugin_gen.sh YOUR_NAME 
+
 set -e
 set -u
 set -o pipefail
 
-module_dir=core/$1
-mkdir -p $module_dir
+plugin_dir=$CGTOOLS_ROOT/cgtools/plugins/$1
+mkdir -p $plugin_dir
 
-cd $module_dir
+cd $plugin_dir
 touch __init__.py
 
 touch $1.py
