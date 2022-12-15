eb65319b7b94bd26fa1cf712b679f4266d8d80fc
crzorz <crzorz@outlook.com>
2022-12-15 10:40:58 +0800

    🎨 logger优化格式

--- misc/logger.sh
+++ misc/logger.sh
@@ -1,42 +1,49 @@
 #!/usr/bin/env bash
 
+# Created by crzorz on 2022/09/16
+# Copyright © 2022 BaldStudio. All rights reserved.
+
 # debug:0; info:1; warning:2; error:3
 loglevel=0
+log_detail=
 logfile="./$(date +'%F').log"
 
-function log() {
-  local logtype=$1
-  local msg=$2
-  local datetime=$(date +'%F %H:%M:%S')
-  logformat_base="[${logtype}] ${datetime} ${msg}"
-  logformat_detail="[${logtype}] ${datetime} [${FUNCNAME[2]} - $(caller 0 | awk '{print$1}')] ${msg}"
-
+log() {
+  local msg
+  local logtype
+  logtype=$1
+  msg=$2
+  datetime=$(date +'%F %H:%M:%S')
+  logformat="[${logtype}] ${msg}"
+  if [[ ${log_detail} == 1 ]]; then
+    logformat="[${logtype}] ${datetime} [${FUNCNAME[2]} - $(caller 0 | awk '{print$1}')] ${msg}"
+  fi
   {
-  case $logtype in
-  DEBUG)
-    [[ $loglevel -le 0 ]] && echo -e "\033[37m${logformat_detail}\033[0m" ;;
-  INFO)
-    [[ $loglevel -le 1 ]] && echo -e "\033[32m${logformat_base}\033[0m" ;;
-  WARNING)
-    [[ $loglevel -le 2 ]] && echo -e "\033[33m${logformat_detail}\033[0m" ;;
-  ERROR)
-    [[ $loglevel -le 3 ]] && echo -e "\033[31m${logformat_detail}\033[0m" ;;
-  esac
+    case $logtype in
+      DEBUG)
+        [[ $loglevel -le 0 ]] && echo -e "\033[37m${logformat}\033[0m" ;;
+      INFO)
+        [[ $loglevel -le 1 ]] && echo -e "\033[32m${logformat}\033[0m" ;;
+      WARNING)
+        [[ $loglevel -le 2 ]] && echo -e "\033[33m${logformat}\033[0m" ;;
+      ERROR)
+        [[ $loglevel -le 3 ]] && echo -e "\033[31m${logformat}\033[0m" ;;
+    esac
   } #| tee -a $logfile
 }
 
-function log_debug() {
+log_debug() {
   log DEBUG "$*"
 }
 
-function log_info() {
+log_info() {
   log INFO "$*"
 }
 
-function log_warning() {
+log_warning() {
   log WARNING "$*"
 }
 
-function log_error() {
+log_error() {
   log ERROR "$*"
 }
